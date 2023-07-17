01ee29437b957e81c488d9a1024ac4b1664c2df4
Tommy Au <smarttommyau@gmail.com>
2023-07-18 00:17:49 +0800

    Update setup-ssh.sh

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -1,3 +1,10 @@
 read -p "Enter user:server: " sshcommand
-ssh-keygen
+read -p "Do you want to generate ssh-keygen? (y/n) " yn
+
+case $yn in 
+	[yY] ) echo OK, we will generate a new key;
+        ssh-keygen;
+		break;;
+	* )  
+esac
 ssh-copy-id $sshcommand
